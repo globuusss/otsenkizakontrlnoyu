1f06b41a5be833c09df67fbabaf8ae38e9dc789b
globuusss <149681009+globuusss@users.noreply.github.com>
2023-11-02 17:06:06 +0500

    f
    f

---
+++ script.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = Math.floor(Math.random() * val);
+    document.getElementById("demotwo").innerHTML = Math.floor(Math.random() * val);
+    document.getElementById("demothree").innerHTML = Math.floor(Math.random() * val);
+  }
+let val = 41
+console.log(demo+demotwo+demothree)
+
+  
+
+console.log(Math.floor(Math.random() * val));
